445f2eb323b902636bd660b225cc94fd10e74547
Bhupal <kotlabhupalreddy2027@gmail.com>
2025-12-18 11:46:33 +0530

    removed backend.tf

--- tf-ansible-stack-dependencies/tf/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "bhupal-spacelift-tf-state"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-2"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
