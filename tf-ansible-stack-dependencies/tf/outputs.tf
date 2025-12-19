dec3e694e10588a6c4b4d43fa38445f57d21b0aa
Bhupal <kotlabhupalreddy2027@gmail.com>
2025-12-19 15:45:08 +0530

    Update outputs.tf

--- tf-ansible-stack-dependencies/tf/outputs.tf
+++ tf-ansible-stack-dependencies/tf/outputs.tf
@@ -1,3 +1,9 @@
-output "aws_instances" {
-  value = [for instance in aws_instance.this : instance.public_ip]
+output "instance_ips" {
+  description = "Public IPs of EC2 instances"
+  value       = aws_instance.example[*].public_ip
+}
+
+output "instance_ids" {
+  description = "Instance IDs"
+  value       = aws_instance.example[*].id
 }
